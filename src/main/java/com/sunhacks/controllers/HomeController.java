129ee932132644c38df7f414471e0cad8748cbb3
Arihant <sinhaarihant@gmail.com>
2018-11-10 18:35:55 -0700

    Controller Class

--- src/main/java/com/sunhacks/controllers/HomeController.java
+++ src/main/java/com/sunhacks/controllers/HomeController.java
@@ -7,9 +7,16 @@ import com.sunhacks.models.Events;
 import com.sunhacks.repository.EventRepository;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import org.joda.time.LocalTime;
+import org.joda.time.format.DateTimeFormat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -38,7 +45,7 @@ public class HomeController {
     }
     
   @RequestMapping(value = "/events")
-  public String getEvents() throws JsonProcessingException, IOException
+  public List<Events> getEvents() throws JsonProcessingException, IOException, ParseException
   {
 	  	RestTemplate restTemplate = new RestTemplate();
     	ObjectMapper mapper = new ObjectMapper();
@@ -72,18 +79,59 @@ public class HomeController {
     	    	
     	    	event.setPlace(objNode.path("_embedded").path("venues").get(0).get("name").toString());
 //    	    	System.out.println(objNode.path("sales").path("public").toString());
-
-	    		event.setEvent_strt_time(objNode.path("sales").path("public").get("startDateTime").toString());
     	    	
+    	    	SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+                Date dt = sdf.parse(objNode.path("sales").path("public").get("startDateTime").asText());
+                long epoch = dt.getTime();
+                event.setEvent_strt_time(epoch/ 1000);
+    	         
 //    	    	ret.add(objNode.get("name").toString());
     	    	ret.add(event);
 //    	        System.out.println(objNode.get("name"));
     	    }
     	}
-    	
-    	
-    	
 //        Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
-        return ret.toString();
+    	
+    	return generate_feasiable_event(ret,"36.7783", "119.4179");
   }
+  
+  public List<Events> generate_feasiable_event(List<Events> event_list, String origin_latitude, String origin_longitude) throws JsonProcessingException, IOException //,long user_strt_time
+	{
+		String requests="https://maps.googleapis.com/maps/api/distancematrix/json?origins="+origin_latitude+","+origin_longitude;
+		RestTemplate restTemplate = new RestTemplate();
+		ObjectMapper mapper = new ObjectMapper();
+		
+		
+		requests+="&destinations=";
+		
+		for(int i=0;i<event_list.size();i++)
+		{
+			requests+=event_list.get(i).getLatitude()+","+event_list.get(i).getLongitude()+"|";
+		}
+		requests=requests.substring(0,requests.length());
+		requests+="&key=AIzaSyAq9QsLNB4AcqvPmLgVhR22CIAznd2Y3uM";
+		
+		System.out.println(requests);
+		
+		ResponseEntity<String> response= restTemplate.getForEntity(requests, String.class);
+		
+		JsonNode root = mapper.readTree(response.getBody());
+	  	JsonNode destinations = root.path("rows").path("events").path("elements"); 	
+	  	
+	  	List<Events> events_fea_list=new ArrayList<Events>();
+	  	
+	  	int i=0;
+	  	for (final JsonNode objNode : destinations)
+	  	{
+	  		long time_taken = Long.parseLong(objNode.get("duration").toString());
+	  		long timestamp = System.currentTimeMillis() / 1000;
+	  		
+	  		if (time_taken+timestamp>event_list.get(i).getEvent_strt_time())// && event_list.get(i).getEvent_strt_time()>user_strt_time)
+	  		{
+	  			events_fea_list.add(event_list.get(i));
+	  		}
+	  		i++;
+	  	}		
+		return events_fea_list;
+	}
 }
